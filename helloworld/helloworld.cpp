a0fc084d1e3c9e7b4c87fac9a1ea63381e9dc2b8
nstig22 <60047732+nstig22@users.noreply.github.com>
2024-01-06 23:04:33 -0600

    Added personal message

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 int addTwoIntegers(int a, int b){
     return a + b;
@@ -9,12 +10,17 @@ int main()
     int a = 0;
     int b = 0;
 
+    std::string name = "test";
+
     std::cout << "Hello World" << std::endl;
 
     std::cout << "Pleas input two integers:";
     std::cin >> a >> b;
 
-    std::cout << addTwoIntegers(a, b);
+    std::cout << "The sum of " << a << " and " << b << " is " << addTwoIntegers(a, b) << std::endl;
 
-
+    std::cout << "Please enter your name:";
+    std::cin.ignore();
+    std::getline(std::cin, name);
+    std::cout << std::endl << "Hello " << name << std::endl;
 }
