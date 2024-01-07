ba43ce9b1f2ab9d67aae99282746ea672686a4a9
nstig22 <60047732+nstig22@users.noreply.github.com>
2024-01-06 22:40:22 -0600

    Added addition calculator

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+int addTwoIntegers(int a, int b){
+    return a + b;
+}
+
 int main()
 {
+    int a = 0;
+    int b = 0;
+
     std::cout << "Hello World" << std::endl;
+
+    std::cout << "Pleas input two integers:";
+    std::cin >> a >> b;
+
+    std::cout << addTwoIntegers(a, b);
+
+
 }
